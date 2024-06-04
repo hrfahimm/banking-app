8f6540ebdcd48eb6d9a2ac90bb463e1381a61945
hrfahimm <fahimtaj10@gmail.com>
2024-06-04 23:37:56 +0600

    null

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+
+            hi
+            cjhjcnksm i niekjo  rw ruwiu
+        </div>
+    )
+}
+
+export default Home
