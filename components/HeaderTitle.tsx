39b6891087b23ab34d760a824cad3370f878fad2
hrfahimm <fahimtaj10@gmail.com>
2024-06-23 22:47:32 +0600

    nothing

--- components/HeaderTitle.tsx
+++ components/HeaderTitle.tsx
@@ -11,8 +11,20 @@ const HeaderTitle: React.FC<HeaderTitleProps> = ({ type, title, subtext, user })
     return (
         <div>
             HeaderTitle
+            <h1>Hi  i am fahim</h1>
+            <div className="m-3c"></div>
         </div>
     )
 }
 
 export default HeaderTitle
+
+
+
+
+
+
+
+
+
+
