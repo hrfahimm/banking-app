20d60b861ade422e88e6d5a6a91af8f202079513
hrfahimm <fahimtaj10@gmail.com>
2024-06-28 22:28:50 +0600

    d

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const TotalBalanceBox = ({ accounts = [], totalBanks, totalCurrentBalance }: TotlaBalanceBoxProps) => {
     return (
-        <div>totalBalanceBox</div>
+        <div> totalBalanceBoxtotalBalanceBoxtotalBalanceBox <div> totalBalanceBoxtotalBalanceBoxtotalBalanceBox</div>
+            <div> totalBalanceBoxtotalBalanceBoxtotalBalanceBox</div>
+            <div className="d rf ti  ">
+                <h1 className="h0">hi my name is fahim
+                    whate is yuor name  
+                    j r
+                </h1>
+            </div>
+
+        </div>
+
     )
 }
 
